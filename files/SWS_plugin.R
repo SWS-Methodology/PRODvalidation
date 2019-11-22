588534c2b33568363989427db7e9632b6f40202f
Christian Mongeau <chrMongeau@users.noreply.github.com>
2019-11-22 11:29:25 +0100

    Update documentation

---
+++ files/SWS_plugin.R
@@ -0,0 +1,8 @@
+# Dummy plugin
+
+# This is the `production_validation` plugin. Its main goal is to obtain
+# the token so that the produciton validation tool can be used.
+
+# Produciton validatio tool:
+# https://github.com/SWS-Methodology/PRODvalidation
+
